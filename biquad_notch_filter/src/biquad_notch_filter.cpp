85d537bf23da4b042634f4fa235f422ea5db587b
branbick <84204053+branbick@users.noreply.github.com>
2022-05-17 11:35:48 -0400

    Started implementing filter realization

--- biquad_notch_filter/src/biquad_notch_filter.cpp
+++ biquad_notch_filter/src/biquad_notch_filter.cpp
@@ -6,15 +6,10 @@
 
 BiquadNotchFilter::BiquadNotchFilter(const double kCenterFreq,
                                      const double kBandwidth,
-                                     const double kNotchDepth)
+                                     const double kNotchDepth,
+                                     const double kSamplingPeriod)
 {
-    setDampingRatios(kCenterFreq, kBandwidth, kNotchDepth);
-}
-
-bool BiquadNotchFilter::setDampingRatios(const double kCenterFreq,
-                                         const double kBandwidth,
-                                         const double kNotchDepth)
-{
+    // Set damping ratios
     try
     {
         // Check for invalid parameters
@@ -41,29 +36,61 @@ bool BiquadNotchFilter::setDampingRatios(const double kCenterFreq,
             exceptionMsg = "d must be > -20*log10(1/sqrt(2)) dB";
             exceptionOccurred = true;
         }
+        else if (kSamplingPeriod <= 0.0)
+        {
+            exceptionMsg = "T must be > 0 s";
+            exceptionOccurred = true;
+        }
 
         if (exceptionOccurred)
             throw (exceptionMsg += ". Damping ratios not properly set.");
 
-        // All parameters are valid. Properly set damping ratios.
+        // All parameters are valid. Proceed.
         const double kConst {(kBandwidth + std::sqrt(kBandwidth * kBandwidth
             + 4 * kCenterFreq * kCenterFreq)) / (2 * kCenterFreq)};
         const double kConstSq {kConst * kConst};
-        dampingRatios_.denominator = std::sqrt((2 - kConstSq - 1 / kConstSq) /
-            (4 * (2 * std::pow(10.0, -kNotchDepth / 10) - 1)));
-        dampingRatios_.numerator = std::pow(10.0, -kNotchDepth / 20)
-            * dampingRatios_.denominator;
-
-        return true;
+        dampingRatios_.den = std::sqrt((2 - kConstSq - 1 / kConstSq)
+            / (4 * (2 * std::pow(10.0, -kNotchDepth / 10) - 1)));
+        dampingRatios_.num = std::pow(10.0, -kNotchDepth / 20)
+            * dampingRatios_.den;
     }
     catch (const std::string kExceptionMsg)
     {
         std::cerr << "ERROR: " << kExceptionMsg << std::endl;
-        return false;
+        // TODO: Stop program execution
     }
+
+    // Set coefficients
+    const double kWcTimesT {kCenterFreq * kSamplingPeriod};
+    const double kSinWcTimesT {std::sin(kWcTimesT)};
+    const double kZetaNumTimesSinWcTimesT {dampingRatios_.num * kSinWcTimesT};
+    const double kZetaDenTimesSinWcTimesT {dampingRatios_.den * kSinWcTimesT};
+
+    const double kAlpha0 {1 + kZetaDenTimesSinWcTimesT};
+
+    coefficients_.b0 = (1 + kZetaNumTimesSinWcTimesT) / kAlpha0;
+    coefficients_.b1 = -2 * std::cos(kWcTimesT) / kAlpha0;
+    coefficients_.b2 = (1 - kZetaNumTimesSinWcTimesT) / kAlpha0;
+    coefficients_.a1 = coefficients_.b1;
+    coefficients_.a2 = (1 - kZetaDenTimesSinWcTimesT) / kAlpha0;
 }
 
-BiquadDampingRatios BiquadNotchFilter::getDampingRatios() const
+BiquadParams::DampingRatios BiquadNotchFilter::getDampingRatios() const
 {
     return dampingRatios_;
+}
+
+BiquadParams::Coefficients BiquadNotchFilter::getCoefficients() const
+{
+    return coefficients_;
+}
+
+void BiquadNotchFilter::resetStates()
+{
+    states_ = BiquadParams::States();
+}
+
+double filterSignal(const double kIn0)
+{
+    // TODO
 }
