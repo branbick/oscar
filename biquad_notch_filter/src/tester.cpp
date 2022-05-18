7838482d0a5c17fbcc6dda68170a603d4c74e374
branbick <84204053+branbick@users.noreply.github.com>
2022-05-18 09:11:35 -0400

    Refactored tester.cpp

--- biquad_notch_filter/src/tester.cpp
+++ biquad_notch_filter/src/tester.cpp
@@ -1,7 +1,30 @@
+// Build command: g++ tester.cpp biquad_notch_filter.cpp -Wall -Wextra -Wpedantic -std=c++17 -o tester.out
+
 #include "biquad_notch_filter.hpp"
 
-#include <string>
 #include <iostream>
+#include <string>
+
+void printDampingRatios(const BiquadNotchFilter& kBiquadNotchFilter)
+{
+    const BiquadParams::DampingRatios kDampingRatios {
+        kBiquadNotchFilter.getDampingRatios()};
+    std::cout << "Damping ratios:" << std::endl;
+    std::cout << "- num: " << kDampingRatios.num << std::endl;
+    std::cout << "- den: " << kDampingRatios.den << std::endl;
+}
+
+void printCoefficients(const BiquadNotchFilter& kBiquadNotchFilter)
+{
+    const BiquadParams::Coefficients kCoefficients {
+        kBiquadNotchFilter.getCoefficients()};
+    std::cout << "Coefficients:" << std::endl;
+    std::cout << "- b0: " << kCoefficients.b0 << std::endl;
+    std::cout << "- b1: " << kCoefficients.b1 << std::endl;
+    std::cout << "- b2: " << kCoefficients.b2 << std::endl;
+    std::cout << "- a1: " << kCoefficients.a1 << std::endl;
+    std::cout << "- a2: " << kCoefficients.a2 << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
@@ -9,33 +32,17 @@ int main(int argc, char* argv[])
     {
         if (argc == 5)
         {
-            // Construct filter
             BiquadNotchFilter biquadNotchFilter{std::stod(argv[1]),
                                                 std::stod(argv[2]),
                                                 std::stod(argv[3]),
                                                 std::stod(argv[4])};
-
-            // Get and print damping ratios
-            const BiquadParams::DampingRatios kDampingRatios {
-                biquadNotchFilter.getDampingRatios()};
-            std::cout << "Damping ratios:" << std::endl;
-            std::cout << "- num: " << kDampingRatios.num << std::endl;
-            std::cout << "- den: " << kDampingRatios.den << std::endl;
-
-            // Get and print coefficients
-            const BiquadParams::Coefficients kCoefficients {
-                biquadNotchFilter.getCoefficients()};
-            std::cout << std::endl << "Coefficients:" << std::endl;
-            std::cout << "- b0: " << kCoefficients.b0 << std::endl;
-            std::cout << "- b1: " << kCoefficients.b1 << std::endl;
-            std::cout << "- b2: " << kCoefficients.b2 << std::endl;
-            std::cout << "- a1: " << kCoefficients.a1 << std::endl;
-            std::cout << "- a2: " << kCoefficients.a2 << std::endl;
+            printDampingRatios(biquadNotchFilter);
+            printCoefficients(biquadNotchFilter);
 
             return 0;
         }
 
-        throw "Enter--in order--wc (rad/s), wb (rad/s), d (+dB), and T (s)";
+        throw "Enter wc (rad/s), wb (rad/s), d (+dB), and T (s)--in order";
     }
     catch (const char* const kExceptionMsg)
     {
