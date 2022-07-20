1ace781580ff5c6fb203111dd6b8027069128039
bbick <84204053+branbick@users.noreply.github.com>
2022-07-20 16:16:49 -0400

    Added calcMagAndPhase to MATLAB mock-up. Need to proof and review results.

---
+++ sys_id_sine_sweep/src/test/matlab_mock_up/calcMagAndPhase.m
@@ -0,0 +1,43 @@
+function freqResponse = calcMagAndPhase(kOutputSignal, kAmplitude, ...
+   kAngFreqs, kSamplesPerFreq, kSamplingPeriod, kCyclesPerFreq, ...
+   kCyclesToIgnorePerFreq)
+
+   kNumFreqs = length(kAngFreqs);
+   samplesToIgnorePerFreq = NaN(1, kNumFreqs);
+   kConst = 2 * pi / kSamplingPeriod * kCyclesToIgnorePerFreq;  % (rad/s)
+   for i = 1:kNumFreqs
+       samplesToIgnorePerFreq(i) = floor(kConst / kAngFreqs(i) + 1);
+   end
+
+   dynamicSampleSize = 0;
+   freqResponse.magnitude = NaN(1, kNumFreqs);
+   freqResponse.phase = NaN(1, kNumFreqs);
+   kRadToDeg = 180 / pi;  % (deg/rad)
+   for i = 1:kNumFreqs
+      dynamicSampleSize = dynamicSampleSize + kSamplesPerFreq(i);
+      kStartSampleIndex = dynamicSampleSize - kSamplesPerFreq(i) ...
+         + samplesToIgnorePerFreq(i) + 1;
+      kAngFreq = kAngFreqs(i);  % (rad/s)
+      inPhaseIntegrand1 = NaN(1, dynamicSampleSize - kStartSampleIndex + 1);
+      quadratureIntegrand1 = NaN(1, dynamicSampleSize - kStartSampleIndex + 1);
+      for j = kStartSampleIndex:dynamicSampleSize
+         inPhaseIntegrand1(j - kStartSampleIndex + 1) = kOutputSignal(j) ...
+            * sin(kAngFreq * j * kSamplingPeriod);
+         quadratureIntegrand1(j - kStartSampleIndex + 1) = kOutputSignal(j) ...
+            * cos(kAngFreq * j * kSamplingPeriod);
+      end
+      
+      % START MATLAB-specific code
+      timePoints = (0 : length(inPhaseIntegrand1) - 1) * kSamplingPeriod;
+      % END MATLAB-specific code
+      kInPhaseCoeff1 = 2 / ((kCyclesPerFreq - kCyclesToIgnorePerFreq) ...
+         * kSamplingPeriod) * trapz(timePoints, inPhaseIntegrand1);
+      kQuadratureCoeff1 = 2 / ((kCyclesPerFreq - kCyclesToIgnorePerFreq) ...
+         * kSamplingPeriod) * trapz(timePoints, quadratureIntegrand1);
+      
+      freqResponse.magnitude(i) = 20 * log10(sqrt(kInPhaseCoeff1^2 ...
+         + kQuadratureCoeff1^2) / kAmplitude);
+      freqResponse.phase(i) = kRadToDeg * atan2(kQuadratureCoeff1, ...
+         kInPhaseCoeff1);
+
+end
--- sys_id_sine_sweep/src/test/matlab_mock_up/tester.m
+++ sys_id_sine_sweep/src/test/matlab_mock_up/tester.m
@@ -4,25 +4,29 @@ clc
 
 %% Inputs
 kMinFreq = 10.0;  % (rad/s)
-kMmaxFreq = 100.0;  % (rad/s)
+kMaxFreq = 100.0;  % (rad/s)
 kNumFreqs = 5;
 
 kCyclesPerFreq = 5;
-kSamplingFreq = 50.0;  % Must be >= kMmaxFreq / pi (Hz)
+kSamplingFreq = 50.0;  % Must be >= kMaxFreq / pi (Hz)
 
 kAmplitude = 0.5;
 
 kCyclesToIgnorePerFreq = 2;  % Must be < kCyclesPerFreq
 
-% Print options: true == ON / "please print"; false == OFF / "don't print"
-PRINT_TEST1 = true;
-PRINT_TEST2 = true;
+% MATLAB-specific
+% - Print options: true == ON / "please print"; false == OFF / "don't print"
+PRINT_TEST1 = false;
+PRINT_TEST2 = false;
 PRINT_TEST3 = false;
-PRINT_TEST4 = false;
-PRINT_TEST5 = false;
+PRINT_TEST4 = true;
+
+% - Parameters for system being "identified"
+naturalFreq = 100;  % (rad/s)
+dampingRatio = 0.5;
 
 %% Generate and optionally print a logarithmically spaced angular frequency vector
-kAngFreqs = logspace(log10(kMinFreq), log10(kMmaxFreq), kNumFreqs);
+kAngFreqs = logspace(log10(kMinFreq), log10(kMaxFreq), kNumFreqs);
 
 if PRINT_TEST1
    fprintf('logSpace test:\n')
@@ -56,4 +60,40 @@ if PRINT_TEST3
    end
    fprintf(inputSignalData, '%.6f', kInputSignal(end));
    fclose(inputSignalData);
+end
+
+%% Calculate and optionally print the corresponding frequency response: magnitude and phase
+% MATLAB-specific code
+% - lsim
+time = (0 : length(kInputSignal) - 1) * kSamplingPeriod;  % (s)
+s = tf('s');
+sysBeingIDed = naturalFreq^2 / (s^2 + 2 * dampingRatio * naturalFreq * s ...
+   + naturalFreq^2);
+outputSignal = lsim(sysBeingIDed, kInputSignal, time)';
+
+% - Plots
+plot(time, kInputSignal, time, outputSignal)
+xlabel('Time (s)'), ylabel('Signal')
+legend('in', 'out')
+grid on, grid minor
+figure; bode(sysBeingIDed)
+xlim([kMinFreq kMaxFreq])
+grid on, grid minor
+
+% C++ code
+kFreqResponse = calcMagAndPhase(outputSignal, kAmplitude, kAngFreqs, ...
+   kSamplesPerFreq, kSamplingPeriod, kCyclesPerFreq, kCyclesToIgnorePerFreq);
+
+if PRINT_TEST4
+   fprintf('calcMagAndPhase test:\n')
+   fprintf('- magnitude: ')
+   kMagAndPhaseSize = length(kFreqResponse.magnitude);
+   for i = 1:kMagAndPhaseSize
+      fprintf('%.4f ', kFreqResponse.magnitude(i))
+   end
+   fprintf('\n- phase: ')
+   for i = 1:kMagAndPhaseSize
+      fprintf('%.4f ', kFreqResponse.phase(i))
+   end
+   fprintf('\n')
 end
