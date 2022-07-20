ef719cc57f485552cc51856b43956c4fc2e6881d
branbick <84204053+branbick@users.noreply.github.com>
2022-07-20 09:28:18 -0400

    Cleaned up tester.cpp for simplified var updates during testing

--- sys_id_sine_sweep/src/test/tester.cpp
+++ sys_id_sine_sweep/src/test/tester.cpp
@@ -8,9 +8,23 @@
 
 int main()
 {
+
+    // Inputs
+    constexpr double kMinFreq {10.0};  // (rad/s)
+    constexpr double kMmaxFreq {100.0};  // (rad/s)
+    constexpr int kNumFreqs {5};
+
+    constexpr int kCyclesPerFreq {5};
+    constexpr double kSamplingFreq {50.0};  // Must be >= kMmaxFreq / pi (Hz)
+
+    constexpr double kAmplitude {0.5};
+
+    constexpr int kCyclesToIgnorePerFreq {2};  // Must be < kCyclesPerFreq
+
     // Generate and optionally print a logarithmically spaced angular frequency
     // vector
-    const std::vector<double> kAngFreqs {logSpace(10.0, 100.0, 5)};  // (rad/s)
+    const std::vector<double> kAngFreqs {logSpace(kMinFreq, kMmaxFreq,
+        kNumFreqs)};  // (rad/s)
 
 #ifdef PRINT_TEST1
     std::cout << "logSpace test:" << std::endl;
@@ -21,9 +35,8 @@ int main()
 
     // Calculate and optionally print the number of samples for each element of
     // kAngFreqs
-    constexpr double kSamplingFreq {100.0};  // (Hz)
-    const std::vector<int> kSamplesPerFreq {
-        calcSamplesPerFreq(kAngFreqs, 8, kSamplingFreq)};
+    const std::vector<int> kSamplesPerFreq {calcSamplesPerFreq(kAngFreqs,
+        kCyclesPerFreq, kSamplingFreq)};
 
 #ifdef PRINT_TEST2
     std::cout << "calcSamplesPerFreq test:" << std::endl;
@@ -34,13 +47,12 @@ int main()
 
     // Generate and optionally print (to a .csv file) a corresponding input
     // signal
-    constexpr double kAmplitude {0.5};
     constexpr double kSamplingPeriod {1 / kSamplingFreq};  // (s)
-    const std::vector<double> kInputSignal {generateInputSignal(
-        kAmplitude, kAngFreqs, kSamplesPerFreq, kSamplingPeriod)};
+    const std::vector<double> kInputSignal {generateInputSignal(kAmplitude,
+        kAngFreqs, kSamplesPerFreq, kSamplingPeriod)};
 
 #ifdef PRINT_TEST3
-    std::ofstream inputSignalData {"data/input.csv"};
+    std::ofstream inputSignalData {"signal_data/input.csv"};
     const int kInputSignalSizeMinus1 {static_cast<int>(kInputSignal.size())
         - 1};
     for (int i {0}; i < kInputSignalSizeMinus1; i++)
@@ -54,7 +66,7 @@ int main()
     //
     // MATLAB code:
     // input = csvread('input.csv');
-    // fs = 100;  % (Hz)
+    // fs = 50;  % (Hz)
     // t = [0 : length(input) - 1] / fs;  % (s)
     // wn = 100;  % (rad/s)
     // zeta = 0.5;
@@ -65,7 +77,7 @@ int main()
     // plot(t, input), xlabel('Time (s)'), ylabel('Signal')
     // hold on; plot(t, output); legend('in', 'out');
     // figure; bode(G)
-    std::ifstream outputSignalData {"data/output.dat"};
+    std::ifstream outputSignalData {"signal_data/output.dat"};
     const int kInputSignalSize {static_cast<int>(kInputSignal.size())};
     std::vector<double> outputSignal(kInputSignalSize);
     int outputSignalIndex {0};
@@ -78,13 +90,13 @@ int main()
     }
     outputSignalData.close();
     const FreqResponse kFreqResponse {calcMagAndPhase(outputSignal, kAmplitude,
-        kAngFreqs, kSamplesPerFreq, kSamplingPeriod, 5)};
+        kAngFreqs, kSamplesPerFreq, kSamplingPeriod, kCyclesToIgnorePerFreq)};
 
 #ifdef PRINT_TEST4
     std::cout << "calcMagAndPhase test:" << std::endl;
     std::cout << "- magnitude: ";
-    const int kMagAndPhaseSize {
-        static_cast<int>(kFreqResponse.magnitude.size())};
+    const int kMagAndPhaseSize {static_cast<int>(
+        kFreqResponse.magnitude.size())};
     for (int i {0}; i < kMagAndPhaseSize; i++)
         std::cout << kFreqResponse.magnitude.at(i) << " ";
     std::cout << std::endl << "- phase: ";
@@ -102,7 +114,7 @@ int main()
     // t = [0.0:0.1:10.0];  % (s)
     // f = 2*t.^2 - 5*t + 13;
     // for i = 1:length(t), fprintf('%.2f, ', f(i)), end, fprintf('\n')
-    // trapz(t,f)
+    // trapz(t, f)
     std::vector<double> f {13.00, 12.52, 12.08, 11.68, 11.32, 11.00, 10.72, 10.48, 10.28, 10.12, 10.00, 9.92, 9.88, 9.88, 9.92, 10.00, 10.12, 10.28, 10.48, 10.72, 11.00, 11.32, 11.68, 12.08, 12.52, 13.00, 13.52, 14.08, 14.68, 15.32, 16.00, 16.72, 17.48, 18.28, 19.12, 20.00, 20.92, 21.88, 22.88, 23.92, 25.00, 26.12, 27.28, 28.48, 29.72, 31.00, 32.32, 33.68, 35.08, 36.52, 38.00, 39.52, 41.08, 42.68, 44.32, 46.00, 47.72, 49.48, 51.28, 53.12, 55.00, 56.92, 58.88, 60.88, 62.92, 65.00, 67.12, 69.28, 71.48, 73.72, 76.00, 78.32, 80.68, 83.08, 85.52, 88.00, 90.52, 93.08, 95.68, 98.32, 101.00, 103.72, 106.48, 109.28, 112.12, 115.00, 117.92, 120.88, 123.88, 126.92, 130.00, 133.12, 136.28, 139.48, 142.72, 146.00, 149.32, 152.68, 156.08, 159.52, 163.00};
 
 #ifdef PRINT_TEST5
