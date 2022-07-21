39d0c9bf2ca36213920776f6a5957a61da995f76
bbick <84204053+branbick@users.noreply.github.com>
2022-07-21 10:49:05 -0400

    MATLAB mock-up: minor refactoring and comment additions

--- sys_id_sine_sweep/src/test/matlab_mock_up/calcMagAndPhase.m
+++ sys_id_sine_sweep/src/test/matlab_mock_up/calcMagAndPhase.m
@@ -2,39 +2,52 @@ function freqResponse = calcMagAndPhase(kOutputSignal, kAmplitude, ...
    kAngFreqs, kSamplesPerFreq, kSamplingPeriod, kCyclesPerFreq, ...
    kCyclesToIgnorePerFreq)
 
+   % TODO: Clean up comments
+
+   % Calculate the number of samples to ignore--which correspond to the
+   % transient response of kOutputSignal--for each element of kAngFreqs
    kNumFreqs = length(kAngFreqs);
-   samplesToIgnorePerFreq = NaN(1, kNumFreqs);
+   samplesToIgnorePerFreq = zeros(1, kNumFreqs);
    kConst = 2 * pi / kSamplingPeriod * kCyclesToIgnorePerFreq;  % (rad/s)
    for i = 1:kNumFreqs
-       samplesToIgnorePerFreq(i) = floor(kConst / kAngFreqs(i) + 1);
+      samplesToIgnorePerFreq(i) = floor(kConst / kAngFreqs(i) + 1);
    end
+      % TODO: Ensure that samplesToIgnorePerFreq(i) < kSamplesPerFreq(i)
+      % before proceeding
 
+   % Calculate the frequency response: magnitude and phase
    dynamicSampleSize = 0;
-   freqResponse.magnitude = NaN(1, kNumFreqs);
-   freqResponse.phase = NaN(1, kNumFreqs);
+   freqResponse.magnitude = zeros(1, kNumFreqs);
+   freqResponse.phase = zeros(1, kNumFreqs);
    kRadToDeg = 180 / pi;  % (deg/rad)
    for i = 1:kNumFreqs
+      % Calculate the integrands of the first in-phase and quadrature Fourier-
+      % series coefficients--i.e., b1 and a1, respectively
       dynamicSampleSize = dynamicSampleSize + kSamplesPerFreq(i);
       kStartSampleIndex = dynamicSampleSize - kSamplesPerFreq(i) ...
          + samplesToIgnorePerFreq(i) + 1;
       kAngFreq = kAngFreqs(i);  % (rad/s)
-      inPhaseIntegrand1 = NaN(1, dynamicSampleSize - kStartSampleIndex + 1);
-      quadratureIntegrand1 = NaN(1, dynamicSampleSize - kStartSampleIndex + 1);
+      inPhaseIntegrand1 = zeros(1, dynamicSampleSize - kStartSampleIndex + 1);
+      quadratureIntegrand1 = zeros(1, dynamicSampleSize - kStartSampleIndex ...
+         + 1);
       for j = kStartSampleIndex:dynamicSampleSize
+         % (j - 1) * kSamplingPeriod == time (s)
          inPhaseIntegrand1(j - kStartSampleIndex + 1) = kOutputSignal(j) ...
             * sin(kAngFreq * (j - 1) * kSamplingPeriod);
          quadratureIntegrand1(j - kStartSampleIndex + 1) = kOutputSignal(j) ...
             * cos(kAngFreq * (j - 1) * kSamplingPeriod);
       end
       
-      % Start of MATLAB-specific code
+      % Calculate b1 and a1
+      % - Start of MATLAB-specific code
       timePoints = (0 : length(inPhaseIntegrand1) - 1) * kSamplingPeriod;
-      % End of MATLAB-specific code
+      % - End of MATLAB-specific code
       kInPhaseCoeff1 = 2 / ((kCyclesPerFreq - kCyclesToIgnorePerFreq) ...
          * kSamplingPeriod) * trapz(timePoints, inPhaseIntegrand1);
       kQuadratureCoeff1 = 2 / ((kCyclesPerFreq - kCyclesToIgnorePerFreq) ...
          * kSamplingPeriod) * trapz(timePoints, quadratureIntegrand1);
       
+      % Calculate the magnitude and phase
       freqResponse.magnitude(i) = 20 * log10(sqrt(kInPhaseCoeff1^2 ...
          + kQuadratureCoeff1^2) / kAmplitude);
       freqResponse.phase(i) = kRadToDeg * atan2(kQuadratureCoeff1, ...
--- sys_id_sine_sweep/src/test/matlab_mock_up/generateInputSignal.m
+++ sys_id_sine_sweep/src/test/matlab_mock_up/generateInputSignal.m
@@ -1,8 +1,14 @@
 function inputSignal = generateInputSignal(kAmplitude, kAngFreqs, ...
    kSamplesPerFreq, kSamplingPeriod)
 
-   inputSignal = NaN(1, sum(kSamplesPerFreq));
+   inputSignal = zeros(1, sum(kSamplesPerFreq));
    
+   % Calculate the elemental values of inputSignal by progressing through
+   % kAngFreqs, advancing to kAngFreqs(i + 1) once the number of samples
+   % collected for kAngFreqs(i)--i.e., numSamples--equals kSamplesPerFreq(i).
+   % Note that time also corresponds to kAngFreqs(i), and resetting it to 0.0
+   % once the aforementioned equivalence is reached ensures each "new" sinusoid
+   % starts with a range value of 0.0.
    freqIndex = 1;
    time = 0.0;  % (s)
    numSamples = 0;
